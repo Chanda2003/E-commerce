62f9f4eab4569ea7f7145760f565f6451f962ab9
Chanda2003 <venumadhavanchanda@gmail.com>
2024-04-19 16:44:40 +0530

    favicon change

--- src/E-Commerce-project/Card.js
+++ src/E-Commerce-project/Card.js
@@ -80,7 +80,7 @@ products.map((eachobject)=>{
     </Card.Text> */}
     <div style={{fontSize:"20px",color:"black"}}><span style={{fontWeight:"bold",fontSize:"25px"}}>Price :</span> ₹  {eachobject.price}</div>
     <br></br>
-    <Button variant="primary" style={{width:"150px",margin:"2px"}} onClick={()=>send(eachobject)}>Add to card</Button>
+    <Button variant="primary" style={{width:"150px",margin:"2px"}} onClick={()=>send(eachobject)}>Add to cart</Button>
     {/* <br></br>  */}
     <Link to={`/productdetails/buy/${eachobject.id}`} style={{textDecoration:"none",color:"white"}}><Button variant="primary" style={{width:"100px"}} onClick={()=>send(eachobject)}>Buy Now</Button></Link>
   </Card.Body>
